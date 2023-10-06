231d24a1f757be75aac89f77bba3a8ba6f0f1464
loginVeb <loginVeb@yandex.ru>
2023-10-06 19:15:19 +0300

    start

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import { VitePWA as pwa } from "vite-plugin-pwa";
+
+
+import manifest from "./locmanifest.json" assert { type: "json" };
+//import manifest from "./locmanifest.json";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    
+  plugins: [
+  react({
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            {
+              displayName: true,
+              fileName: false
+            }
+          ]
+        ]
+      }
+    })  ,
+    pwa({
+      strategies: "injectManifest",
+      srcDir: "",
+      filename: "service-worker.js",
+      manifest,
+      devOptions: {
+        enabled: true,
+        type: 'module',
+      },
+    }),
+  ],
+});
